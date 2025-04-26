4022d1812b1be2a4e92e99240a9891dbf68788fd
Sandysanthosh9 <127300006+Sandysanthosh9@users.noreply.github.com>
2025-04-26 18:07:46 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,31 @@
-const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-mongoose.connect("mongodb+srv://santhosh9726:<sandy9726>@digital-menu.km55hdy.mongodb.net/?retryWrites=true&w=majority&appName=digital-menu", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch(err => {
-    console.error("MongoDB connection error: ", err);
-  });
+const uri = "mongodb+srv://santhosh9726:pLmmuZbmZ8Tvz36x@digital-menu.km55hdy.mongodb.net/?retryWrites=true&w=majority&appName=digital-menu";
 
-module.exports = mongoose;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let dbInstance; // to reuse same DB connection
+
+async function connectDB() {
+  if (dbInstance) {
+    return dbInstance;
+  }
+
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB!");
+    dbInstance = client.db("digital-menu"); // 👉 Replace with your database name!
+    return dbInstance;
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // Exit if connection fails
+  }
+}
+
+module.exports = connectDB;
