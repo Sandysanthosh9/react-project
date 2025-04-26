72de73b85d209479d8b080dcc819dfb119eafbf9
Sandysanthosh9 <127300006+Sandysanthosh9@users.noreply.github.com>
2025-04-26 18:30:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,9 @@ app.use("/uploads", express.static("uploads"));
 const menuRoutes = require("./routes/menu");
 app.use("/api/menu", menuRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+// FIX THIS:
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
