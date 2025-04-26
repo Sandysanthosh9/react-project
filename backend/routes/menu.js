cacb59e7c2b6186b16118380b51514e724834b22
Sandysanthosh9 <127300006+Sandysanthosh9@users.noreply.github.com>
2025-04-26 18:07:06 +0530

    Update menu.js

--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const menuController = require("../controller/MenuItemController");
+const menuController = require("../models/MenuItem");
 
-// Set up multer for handling file uploads
 const upload = multer({ dest: "uploads/" });
 
-// Routes
 router.get("/", menuController.getAllItems);
 router.post("/", upload.single("image"), menuController.addItem);
 
